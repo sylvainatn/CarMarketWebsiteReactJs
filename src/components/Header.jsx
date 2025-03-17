ac20747c8dbda66badb071f15597c22d0c56a755
Sylvain ANTON <sylvainanton77@gmail.com>
2025-03-17 19:10:35 +0100

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaCar } from 'react-icons/fa';
 import '../css/Header.css'; // css
 
